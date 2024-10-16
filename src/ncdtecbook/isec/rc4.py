bed517f6e5855864890e188e1ba580f8353ac864
Suhel Hammoud <suhel.hammoud@gmail.com>
2024-10-16 13:55:40 +0300

    gridbox in ipywidgets

--- src/ncdtecbook/isec/rc4.py
+++ src/ncdtecbook/isec/rc4.py
@@ -1,4 +1,5 @@
-import ipywidgets as widgets
+from ipywidgets import  GridBox, Layout, Label, Text, Textarea, Button
+
 from IPython.display import display
 
 # Step 1: Key Scheduling Algorithm (KSA)
@@ -55,33 +56,109 @@ def example_rc4():
     # Creating the widgets
 
     # 1. Text input for key
-    key_input = widgets.Text(
+
+    key_label = Label(
+        value='Key:',
+        layout=Layout(width='auto', grid_area='key'),
+    )
+    key_input = Text(
         value='',
         placeholder='Enter encryption key',
-        description='Key:',
+        description='',
+        continuous_update=False,
+        layout=Layout(width='auto', grid_area='keyv')
     )
 
     # 2. Label to show the text_to_bytes of the key in real-time
-    key_bytes = widgets.Label(value="Key Bytes: ")
-
-    # 3. Text input for message
-    message_input = widgets.Text(
+    key_bytes_label = Label(
+        value='Key Bytes:',
+        layout=Layout(width='auto', grid_area='kb')
+    )
+    key_bytes = Textarea(
         value='',
-        placeholder='Enter message',
-        description='Plain Text:',
+        disabled=True,
+        layout=Layout(width='auto', grid_area='kbv')
     )
 
+    # 3. Text input for message
+    message_input_label = Label(
+        value='Plain Text:',
+        layout=Layout(width='auto', grid_area='pt')
+    )
+    message_input = Textarea(
+        value='',
+        placeholder='Enter message',
+        continuous_update=False,
+        disabled=False,
+        layout=Layout(width='auto', grid_area='ptv')
+    )
+
+    
     # 4. Label to show the text_to_bytes of the message in real-time
-    message_bytes = widgets.Label(value="Plain Text Bytes: ")
+    message_bytes_label = Label(
+        value='Plain Text Bytes:',
+        layout=Layout(width='auto', grid_area='ptb')
+    )
+    message_bytes = Textarea(
+        value='',
+        disabled=True,
+        layout=Layout(width='auto', grid_area='ptbv')
+    )
 
-    # 5. Label to show the cipher bytes (output of RC4 function)
-    cipher_bytes = widgets.Label(value="Cipher Bytes: ")
+    # 5. Encryption button
+    btn_encrypt = Button(
+            description="Encrypt",
+            button_style='success',
+            layout=Layout(width='auto', grid_area='btn')
+        )
 
-    cipher_message = widgets.Label(value="Cipher Text: ")
+    # 6. Label to show the cipher bytes (output of RC4 function)
+    cipher_bytes_label = Label(
+        value='Cipher Bytes:',
+        layout=Layout(width='auto', grid_area='ctb')
+    )
 
-    # 6. Label to show the decrypted message (output of bytes_to_text on the cipher bytes)
-    # decrypted_message = widgets.Label(value="Decrypted Message: ")
+    cipher_bytes = Textarea(
+        value='',
+        disabled=True,
+        layout=Layout(width='auto', grid_area='ctbv')
+    )
 
+    cipher_message_label = Label(
+        value='Cipher Text:',
+        layout=Layout(width='auto', grid_area='ct')
+    )
+    cipher_message =  Textarea(
+        value='',
+        disabled=True,
+        layout=Layout(width='auto', grid_area='ctv')
+    )
+
+
+    gb = GridBox(children=[
+        key_label, key_input,
+        key_bytes_label, key_bytes,
+        message_input_label, message_input,
+        message_bytes_label, message_bytes,
+        btn_encrypt,
+        cipher_bytes_label, cipher_bytes,
+        cipher_message_label, cipher_message
+
+        ],
+        layout=Layout(
+            width='80%',
+            grid_template_rows='auto auto auto auto auto auto auto',
+            grid_template_columns='25% 75%',
+            grid_template_areas='''
+            "key keyv"
+            "kb kbv"
+            "pt ptv"
+            "ptb ptbv"
+            "btn btn"
+            "ctb ctbv"
+            "ct ctv"
+            ''')
+       )
     # Function to update the cipher_bytes and decrypted_message when message_input changes
     def update_cipher_and_decrypted(change):
         # Get the new message and key values
@@ -90,22 +167,22 @@ def example_rc4():
         message = message_input.value
         
 
-        key_bytes.value = f"Key Bytes: {text_to_bytes(key)}"
+        key_bytes.value = f"{text_to_bytes(key)}"
 
         # Convert message and key to byte arrays
         message_byte_array = text_to_bytes(message)
         key_byte_array = text_to_bytes(key)
         
         # Update the message bytes label
-        message_bytes.value = f"Plain Text Bytes: {message_byte_array}"
+        message_bytes.value = f"{message_byte_array}"
         
         # Encrypt using RC4
         cipher_byte_array = RC4(key_byte_array, message_byte_array)
         
         # Update the cipher bytes label
-        cipher_bytes.value = f"Cipher Bytes: {cipher_byte_array}"
+        cipher_bytes.value = f"{cipher_byte_array}"
         
-        cipher_message.value = f"Cipher Text: {bytes_to_text(cipher_byte_array)}"
+        cipher_message.value = f"{bytes_to_text(cipher_byte_array)}"
         
         # Decrypt the cipher bytes back to text
         # decrypted = bytes_to_text(RC4(key_byte_array, cipher_byte_array))
@@ -119,13 +196,18 @@ def example_rc4():
     # Link the message_input text change to update cipher_bytes and decrypted_message
     message_input.observe(update_cipher_and_decrypted, names='value')
 
+    btn_encrypt.on_click(update_cipher_and_decrypted)
+
+
     # Display the widgets
     display(
-        key_input,
-        key_bytes,
-        message_input,
-        message_bytes,
-        cipher_bytes,
-        cipher_message,
+        gb,
+        # key_input,
+        # key_bytes,
+        # message_input,
+        # message_bytes,
+        # btn_encrypt,
+        # cipher_bytes,
+        # cipher_message,
         # decrypted_message,
         )
